3b05b4b51a7dc1e28f989122076253b2ae209c28
Nikolai Poverud <nikpov@intern.i-sone.no>
2020-10-29 14:51:51 +0100

    init commit

---
+++ Reset-PasswordViaPureservice.ps1
@@ -0,0 +1,24 @@
+﻿Param(
+    $ticketNumber,
+    $WaitFor
+)
+
+if (!($WaitFor -eq $null)) {
+    Do {
+        $proc = Get-Process -Id $WaitFor
+    }
+    until ($proc -eq $null)
+}
+
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/NikolaiPoverud/PureservicePasswordReset/master/ResetADPassword.ps1" -OutFile "C:\Pureservice\ResetADPassword.ps1" -UseBasicParsing
+
+
+$Path = "C:\Pureservice"
+
+if ($ticketNumber) {
+    Start-Process "powershell.exe" -ArgumentList "-file $Path\ResetADPassword.ps1 -ticketNumber $ticketNumber"
+}
+else {
+    Start-Process "powershell.exe" -ArgumentList "-file $Path\ResetADPassword.ps1"
+}
+
